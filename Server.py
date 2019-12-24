8dc00397b97ad714cbd89190ecc2c08224c4b877
andrey545454 <superkurlin2013@yandex.ru>
2019-12-24 19:16:56 +0500

    Сервер работает в многопоточке

--- Server.py
+++ Server.py
@@ -1,25 +1,39 @@
+import socket
+import threading
 import socketserver
 
 
-class PyServer(socketserver.TCPServer):
+class PyServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     def __init__(self, server_address, RequestHandlerClass):
         super().__init__(server_address, RequestHandlerClass)
 
 
-class TCPHandler(socketserver.BaseRequestHandler):
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
     """
     The request handler class for our server.
     """
+    def setup(self):
+        # Setup connection
+        print("Подключение пользователя к серверу")
+        self.request.sendall("Привет, пользователь :)".encode())
+
     def handle(self):
-        # Send data
-        self.request.sendall("Hello, world".encode())
+        # Handle data
+        self.request.sendall("Ответ от сервера получен".encode())
 
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 50007
 
     # Create the server, binding to localhost on port 50007
-    with PyServer((HOST, PORT), TCPHandler) as server:
+    with PyServer((HOST, PORT), ThreadedTCPRequestHandler) as server:
+        ip, port = server.server_address
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
-        server.serve_forever()
+
+        server_thread = threading.Thread(target=server.serve_forever)
+        server_thread.daemon = True
+        server_thread.start()
+
+        print("Server loop running in thread:", server_thread.name)
+        server_thread.run()
