4e7a5a3523a26260622e1f15f4a9832116226adc
andrey545454 <superkurlin2013@yandex.ru>
2019-12-24 18:25:51 +0500

    Creating a Server class

--- Server.py
+++ Server.py
@@ -1,7 +1,12 @@
 import socketserver
 
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
+class PyServer(socketserver.TCPServer):
+    def __init__(self, server_address, RequestHandlerClass):
+        super().__init__(server_address, RequestHandlerClass)
+
+
+class TCPHandler(socketserver.BaseRequestHandler):
     """
     The request handler class for our server.
     """
@@ -14,7 +19,7 @@ if __name__ == "__main__":
     HOST, PORT = "localhost", 50007
 
     # Create the server, binding to localhost on port 50007
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+    with PyServer((HOST, PORT), TCPHandler) as server:
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
         server.serve_forever()
