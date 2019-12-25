320b01bede54bb951743cf7b35b4fa8deb939f51
andrey545454 <superkurlin2013@yandex.ru>
2019-12-25 19:52:27 +0500

    Переход к самописному серверу

--- client.py
+++ client.py
@@ -4,3 +4,6 @@ HOST = 'localhost'    # The remote host
 PORT = 50007          # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    while True:
+        s.sendall(b"hi")
+        print("Sended :)")
