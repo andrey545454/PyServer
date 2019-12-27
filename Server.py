bc72107bffc26c57b426bdca220038231386918b
andrey545454 <superkurlin2013@yandex.ru>
2019-12-27 19:28:00 +0500

    Сервер с поддержкой многопоточки

--- Server.py
+++ Server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import socketserver
+import selectors
 import time
 import queue
 
@@ -10,8 +11,61 @@ class PyServer():
     Our server
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, addr):
+        # Адрес сервера
+        self.ip = addr[0]
+        self.port = addr[1]
+
+        # Потоки
+        self._threads = []
+
+
+    def __enter__(self):
+        """
+        Создание и настройка сервера
+
+        bind - Привязка сокета к адресу
+        setblocking - Установка блокирующего/неблокирующего режима сокета
+        listen - Макс. количество подключений
+
+        """
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.sock.bind((self.ip, self.port))
+        self.sock.setblocking(False)
+        self.sock.listen(10)
+
+    def run(self):
+
+        while True:
+            try:
+                # Пробуем получить новое сокет подключение
+                conn, addr = self.sock.accept()
+                # Если есть новое подключение, то обрабатываем его
+                if conn:
+                    t = threading.Thread(target=self.client_thread, args=(conn, addr))
+                    self._threads.append(t)
+                    t.start()
+            except:
+                pass
+
+    def client_thread(self, connection, address):
+        """
+        Потоки для клиентов, подключённых к серверу
+        """
+        is_active = True
+        while is_active:
+            pass
+
+        connection.close()
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Закрытие сервера
+        """
+        self.sock.close()
 
 
 if __name__ == "__main__":
@@ -20,4 +74,4 @@ if __name__ == "__main__":
     # Create the server, binding to localhost on port 50007
     server = PyServer((HOST, PORT))
     with server:
-        ip, port = server.server_address
+        server.run()
--- client.py
+++ client.py
@@ -1,9 +1,12 @@
 import socket
+from time import sleep
 
 HOST = 'localhost'    # The remote host
 PORT = 50007          # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    s.setblocking(False)
     while True:
-        s.sendall(b"hi")
-        print("Sended :)")
+        msg = b"hi"
+        s.sendall(msg)
+        sleep(2)
